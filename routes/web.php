42683a6e77d85feef450a6071fe80d33395d1fd6
Octwopuss <yohaneshdr@gmail.com>
2019-03-13 11:14:28 +0700

    test generate ticket otomatis

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('ongoing', 'linkController@ongoing')->name('ongoing');
 Route::get('buat_tiket', 'linkController@create')->name('createTicket');
 Route::get('track', 'linkController@track')->name('trackTicket');
 Route::get('tiket_selesai', 'linkController@finished')->name('finishedTicket');
-Route::get('test', function(){
+Route::get('track_test', function(){
     $stats = DB::table('status_ticket')->where('ticket_id', 1)->get();
 
     return view('test', compact('stats'));
