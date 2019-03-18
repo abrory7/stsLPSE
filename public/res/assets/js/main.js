cb9d8efac9134e181e2572ea77aa0db9541a4963
Taufiq Abrory <abroryrb7@gmail.com>
2019-03-18 12:18:17 +0800

    hapus intro

--- public/res/assets/js/main.js
+++ public/res/assets/js/main.js
@@ -39,7 +39,7 @@ $(window).on('load', function() {
         });
     };
 
-    notify('Selamat Datang di Support Ticketing System LPSE!', 'inverse');
+    // notify('Selamat Datang di Support Ticketing System LPSE!', 'inverse');
     $('.loader-bg').fadeOut('slow');
 
 });
